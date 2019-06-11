56b288da416ee40294493c3fb37b023005512cc7
Dominik Smrž <dominiksmrz@seznam.cz>
2019-06-11 11:25:27 +0200

    train test split

--- moles.py
+++ moles.py
@@ -2,13 +2,13 @@ import zipfile
 import PIL.Image
 import numpy as np
 import pickle
+import sklearn.model_selection
 
-USE_HSV = False
 IMAGE_DIM = 224
 
 with zipfile.ZipFile('skin-cancer-malignant-vs-benign.zip') as zip_handle:
     filenames = list(filter(lambda fn: fn.endswith('.jpg'), zip_handle.namelist()))
-    data = np.zeros(shape=(len(filenames), IMAGE_DIM, IMAGE_DIM, 3), dtype=np.uint8)
+    data = np.zeros(shape=(len(filenames), IMAGE_DIM, IMAGE_DIM, 6), dtype=np.uint8)
     targets = np.zeros(shape=len(filenames), dtype=np.bool)
     for i_file, filename in enumerate(sorted(filenames)):
         with zip_handle.open(filename) as image_file_handle, PIL.Image.open(image_file_handle) as image_handle:
@@ -23,13 +23,20 @@ with zipfile.ZipFile('skin-cancer-malignant-vs-benign.zip') as zip_handle:
                 raise ValueError("Invalid filename")
             targets[i_file] = target
 
-            if USE_HSV:
-                image_handle = image_handle.convert('HSV')
-            pixels = image_handle.load()
+            pixels_rgb = image_handle.load()
+            image_handle_hsv = image_handle.convert('HSV')
+            pixels_hsv = image_handle_hsv.load()
             for x, y in np.ndindex((IMAGE_DIM, IMAGE_DIM)):
-                data[i_file, x, y] = pixels[x, y]
+                data[i_file, x, y] = pixels_rgb[x, y] + pixels_hsv[x, y]
 
-with open('data{}.dat'.format('_HSV' if USE_HSV else ''), 'wb') as filehandle:
-    pickle.dump(data, filehandle)
-with open('targets{}.dat'.format('_HSV' if USE_HSV else ''), 'wb') as filehandle:
-    pickle.dump(targets, filehandle)
+data_train, data_test, targets_train, targets_test = sklearn.model_selection.train_test_split(
+        data, targets, test_size=0.2, random_state=42, shuffle=True, stratify=targets)
+
+with open('data_train.dat', 'wb') as filehandle:
+    pickle.dump(data_train, filehandle)
+with open('targets_train.dat', 'wb') as filehandle:
+    pickle.dump(targets_train, filehandle)
+with open('data_test.dat', 'wb') as filehandle:
+    pickle.dump(data_test, filehandle)
+with open('targets_test.dat', 'wb') as filehandle:
+    pickle.dump(targets_test, filehandle)
